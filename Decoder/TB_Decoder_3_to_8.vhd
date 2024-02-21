3bdb3aed72c87b5b37aa314befa78a4b9181f020
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-02-21 16:21:46 +0530

    initial files

--- Decoder/TB_Decoder_3_to_8.vhd
+++ Decoder/TB_Decoder_3_to_8.vhd
@@ -1,25 +1,26 @@
 ----------------------------------------------------------------------------------
--- Company:
--- Engineer:
---
--- Create Date: 02/20/2024 02:58:47 PM
--- Design Name:
--- Module Name: TBDecoder2to_4 - Behavioral
--- Project Name:
--- Target Devices:
--- Tool Versions:
--- Description:
---
--- Dependencies:
---
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/21/2024 11:59:01 AM
+-- Design Name: 
+-- Module Name: TB_Decoder_3_to_8 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
---
+-- 
 ----------------------------------------------------------------------------------
 
+
 library IEEE;
-use IEEE.STDLOGIC1164.ALL;
+use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -29,24 +30,27 @@ use IEEE.STDLOGIC1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity TBDecoder2to_4 is
+entity TBDecoder3to8 is
 -- Port ( );
-end TBDecoder2to_4;
+end TBDecoder3to8;
 
-architecture Behavioral of TBDecoder2to_4 is
-COMPONENT Decoder2to_4
-Port ( I : in STDLOGICVECTOR (1 downto 0);
+architecture Behavioral of TBDecoder3to8 is
+component Decoder_3_to_8
+
+
+Port ( I : in STD_LOGIC_VECTOR (2 downto 0);
    EN : in STD_LOGIC;
-   Y : out STD*LOGIC*VECTOR (3 downto 0));
-END COMPONENT;
+   Y : out STD_LOGIC_VECTOR (7 downto 0));
+
+end component;
 
 SIGNAL EN : std_logic;
-
-SIGNAL Y : stdlogicvector(3 downto 0);
-SIGNAL I : stdlogicvector(1 downto 0);
+SIGNAL I : std_logic_vector(2 downto 0);
+SIGNAL Y : std_logic_vector(7 downto 0);
 
 begin
-UUT: Decoder2to_4 PORT MAP(
+
+UUT: Decoder_3_to_8 PORT MAP(
 I => I,
 EN => EN,
 Y => Y
@@ -54,26 +58,28 @@ Y => Y
 
 process
 begin
-EN <= '0';
-I(0) <= '1';
-I(1) <= '0';
-WAIT FOR 100ns;
+
 EN <= '1';
 I(0) <= '0';
 I(1) <= '0';
+I(2) <= '0';
 WAIT FOR 100ns;
-I(0) <= '1';
-I(1) <= '0';
-WAIT FOR 100ns;
-I(0) <= '0';
 I(1) <= '1';
+I(2) <= '1';
 WAIT FOR 100ns;
 I(0) <= '1';
+I(2) <= '0';
+WAIT FOR 100ns;
+I(1) <= '0';
+I(2) <= '1';
+WAIT FOR 100ns;
+I(1) <= '1';
+I(2) <= '0';
 WAIT FOR 100ns;
 I(0) <= '0';
 I(1) <= '0';
+I(2) <= '0';
 EN <= '0';
 WAIT;
 end process;
-
 end Behavioral;
